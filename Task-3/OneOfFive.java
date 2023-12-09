748c657e44e9a8537c71fba8fb6e545d10872c14
khaledkretos7 <khaled60.kretos70@gmail.com>
2023-12-09 21:00:52 +0200

    on of five code

--- Task-3/OneOfFive.java
+++ Task-3/OneOfFive.java
@@ -4,19 +4,15 @@ public class OneOfFive {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int max = 0;
-        int min = 0;
         int prime = 0;
         int palindrome = 0;
         int[] arr = new int[n];
-        int maxdiv = 0;
+        int max = 0;
         for (int i = 0; i < n; i++) {
             arr[i] = in.nextInt();
         }
-        for (int i = 0; i < n - 1; i++) {
-            max = Math.max(arr[i], arr[i + 1]);
-            min = Math.min(arr[i], arr[i + 1]);
-
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, arr[i]);
         }
         for (int i = 0; i < n; i++) {
             if (prime(arr[i])) {
@@ -25,16 +21,40 @@ public class OneOfFive {
             if (isPalndrome(arr[i])) {
                 palindrome += 1;
             }
-            if (divisors(arr[i]) >= maxdiv) {
-                maxdiv += arr[i];
+
+        }
+
+        int c = 0;
+        int maxdiv = 0;
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) { // i = 0, 1,2 // 1 2 5 8
+            c = divisors(arr[i]); // 4
+            if (i == 0) {
+                maxdiv = c;
+                index = i;
+            } else if (c > maxdiv) {
+                maxdiv = c;
+                index = i;
+            } else if (c == maxdiv && arr[i] > arr[index]) {
+                index = i;
             }
         }
         System.out.println("The maximum number : " + max);
-        System.out.println("The minimum number : " + min);
+        System.out.println("The minimum number : " + min(arr));
         System.out.println("The number of prime numbers : " + prime);
         System.out.println("The number of palindrome numbers : " + palindrome);
-        System.out.println("The number that has the maximum number of divisors : " + maxdiv);
+        System.out.println("The number that has the maximum number of divisors : " + arr[index]);
         in.close();
+    }
+
+    public static int min(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        return min;
     }
 
     public static int divisors(int num) {
